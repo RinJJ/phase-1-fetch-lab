699eb58ac83d68543c6350e2c8711b0745d40e2b
Jesse Hunter <jjsbizarreadventure@yahoo.com>
2023-02-06 09:28:40 -0600

    using Fetch to get data on GoT books

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(json => renderBooks(json))
   // To pass the tests, don't forget to return your fetch!
   
 }
